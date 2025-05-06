c53f5eae058399bda3c109faa5823152127a5fae
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-05-06 08:35:09 +0100

    Add React Query and Auth providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import AuthProvider from "@/providers/AuthProvider";
+import QueryClientProvider from "@/providers/QueryClientProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,8 +28,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <main>{children}</main>
-        <Toaster />
+        <QueryClientProvider>
+          <AuthProvider>
+            <main>{children}</main>
+            <Toaster />
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
---
+++ providers/AuthProvider.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { SessionProvider } from "next-auth/react";
+import React, { PropsWithChildren } from "react";
+
+const AuthProvider: React.FC<PropsWithChildren> = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
+
+export default AuthProvider;
