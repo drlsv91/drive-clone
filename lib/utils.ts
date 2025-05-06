c3f0fe4aa4e9b27fe93d75e4cba054f4c0dee508
Oluwaleye Victor <oluwasquarelsv@gmail.com>
2025-05-06 17:59:51 +0100

    Add format date util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -78,6 +78,15 @@ export function getFileExtension(mimeType: string): string {
   return mimeTypeToExtension[mimeType] || "";
 }
 
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(date);
+};
+
 export function sanitizeFilename(filename: string): string {
   return filename
     .replace(/[<>:"\/\\|?*\x00-\x1F]/g, "_")
